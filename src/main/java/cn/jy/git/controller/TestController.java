b08e181f66b175cf5b723083fc13e1b3bfe579e0
hq <hq@163.com>
2022-03-31 19:04:09 +0800

    第一次提交 test

--- src/main/java/cn/jy/git/controller/TestController.java
+++ src/main/java/cn/jy/git/controller/TestController.java
@@ -3,5 +3,6 @@ package cn.jy.git.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hello first commit");
+        System.out.println("hello test commit");
     }
 }
